8d21cdf6f71e4ee7b23f9d586ecc77a7895c133a
Jose <jgarciallamas@yahoo.es>
2022-06-23 09:47:04 +0200

    Create the user profile page

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: [
+      "localhost",
+      "bootcamp-jose.s3.eu-west-3.amazonaws.com",
+      "bootcamp-jose.s3.amazonaws.com",
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
---
+++ pages/api/utils.js
@@ -0,0 +1,66 @@
+import prisma from "lib/prisma";
+import { faker } from "@faker-js/faker";
+import AWS from "aws-sdk";
+
+export default async function handler(req, res) {
+  if (req.method !== "POST") return res.end();
+
+  if (req.body.task === "generate_content") {
+    //create 10 users
+    let usersCount = 0;
+
+    while (usersCount < 10) {
+      await prisma.user.create({
+        data: {
+          name: faker.name.findName(),
+          username: faker.internet.userName().toLowerCase(),
+          email: faker.internet.email().toLowerCase(),
+          image: faker.image.avatar(),
+        },
+      });
+      usersCount++;
+    }
+
+    const s3 = new AWS.S3({
+      accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY,
+    });
+
+    const videoURL =
+      "https://bootcamp-jose.s3.eu-west-3.amazonaws.com/SampleVideo_1280x720_1mb.mp4";
+    const thumbnailURL =
+      "https://bootcamp-jose.s3.eu-west-3.amazonaws.com/animals.jpg";
+
+    const users = await prisma.user.findMany();
+
+    const getRandomUser = () => {
+      const randomIndex = Math.floor(Math.random() * users.length);
+      return users[randomIndex];
+    };
+
+    let videosCount = 0;
+    while (videosCount < 20) {
+      await prisma.video.create({
+        data: {
+          title: faker.lorem.words(),
+          thumbnail: thumbnailURL,
+          url: videoURL,
+          length: faker.datatype.number(1000),
+          visibility: "public",
+          views: faker.datatype.number(1000),
+          author: {
+            connect: { id: getRandomUser().id },
+          },
+        },
+      });
+      videosCount++;
+    }
+  }
+
+  if (req.body.task === "clean_database") {
+    // By deleting the user, we also delete its videos, because of how their relation is defined in the schema.
+    await prisma.user.deleteMany({});
+  }
+
+  res.end();
+}
